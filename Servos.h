b42ec62720354c1229e597b0aeabafeb1d5f1d15
Alex Jordan <alexander3223098@gmail.com>
2014-09-20 15:47:15 -0700

    rename the MotorNumber var to ServoNumber in Servos.h
    how embarrasing.

--- Servos.h
+++ Servos.h
@@ -1,8 +1,8 @@
 #include "drivers/common.h"
 #include "I2C.h"
 
-void Servos_SetPosition(tSensors Port, int DaisyChainLevel, int MotorNumber, int Position)
+void Servos_SetPosition(tSensors Port, int DaisyChainLevel, int ServoNumber, int Position)
 {
 	I2C_WritePMW(Port, DaisyChainLevel, 0);
-	I2C_MoveServo(Port, DaisyChainLevel, MotorNumber, Position);
+	I2C_MoveServo(Port, DaisyChainLevel, ServoNumber, Position);
 }
