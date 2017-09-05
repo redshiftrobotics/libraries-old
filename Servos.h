8dba6db670896ac0f32564be6f2fb118a82e8ce9
duncanmckee <duncan@mckee.net>
2017-09-05 16:40:34 -0700

    Remove zinda's legacy

--- Servos.h
+++
@@ -1,8 +0,0 @@
-#include "drivers/common.h"
-#include "I2C.h"
-
-void Servos_SetPosition(tSensors Port, int DaisyChainLevel, int ServoNumber, int Position)
-{
-	I2C_WritePMW(Port, DaisyChainLevel, 0);
-	I2C_MoveServo(Port, DaisyChainLevel, ServoNumber, Position);
-}
